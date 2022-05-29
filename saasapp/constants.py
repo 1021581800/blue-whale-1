9234f94f67dd120c7a3d8fa06ec85a88fecc592b
1021581800 <1021581800@qq.com>
2022-05-29 13:54:05 +0800

    first commit

---
+++ saasapp/constants.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+#我感觉是返回给分页器用的~ 参数
+PAGE_SIZE = 10
+PAGE_QUERY_PARAM = "page"
+PAGE_SIZE_QUERY_PARAM = "size"
+MAX_PAGE_SIZE = 10000
---
+++ saasapp/sops_task/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SopsTaskConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'saasapp.sops_task'
